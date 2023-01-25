1d215316d7dcbde9bdb07442607bee2e3f2482c2
bibek977 <bhattaraibibek91@gmail.com>
2023-01-25 13:00:41 +0545

    tuts simple part

--- scrapy_1/scrapy_1/spiders/qoute_spider.py
+++ scrapy_1/scrapy_1/spiders/qoute_spider.py
@@ -1,13 +1,27 @@
 import scrapy
 from ..items import Scrapy1Item
+from scrapy.http import FormRequest
+from scrapy.utils.response import open_in_browser
 
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
     start_urls = [
-        'https://quotes.toscrape.com/'
+        # 'https://quotes.toscrape.com/'
+        'https://quotes.toscrape.com/login'
     ]
 
     def parse(self, response):
+        token = response.css('form input::attr(value)').extract_first()
+        return FormRequest.from_response(response, formdata ={
+            'csrf_token' : token,
+            'username' : 'nepbee',
+            'password' : 'nepbee123'
+        }, callback=self.login)
+
+
+    def login(self, response):
+
+        open_in_browser(response)
 
         item = Scrapy1Item()
 
@@ -24,7 +38,7 @@ class QuoteSpider(scrapy.Spider):
 
             yield item
 
-        next_page = response.css('li.next a::attr(href)').get()
+        # next_page = response.css('li.next a::attr(href)').get()
 
-        if next_page is not None:
-            yield response.follow(next_page,callback = self.parse)
+        # if next_page is not None:
+        #     yield response.follow(next_page,callback = self.parse)
